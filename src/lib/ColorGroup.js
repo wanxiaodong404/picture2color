829c1dcce3be8aa91811da0e325bc2fc78878147
wanxiaodong <729779978@qq.com>
2020-12-23 16:51:14 +0800

    fix: fixed count bug

--- src/lib/ColorGroup.js
+++ src/lib/ColorGroup.js
@@ -2,7 +2,7 @@
  * @Author: wanxiaodong
  * @Date: 2020-11-26 14:50:42
  * @Last Modified by: wanxiaodong
- * @Last Modified time: 2020-12-23 11:46:41
+ * @Last Modified time: 2020-12-23 16:48:34
  * @Description:
  */
 const Color = require('./Color');
@@ -17,6 +17,7 @@ class ColorGroup extends Count {
         this.__proxy = child || null; // 如果是颜色范围的话，可设置一个颜色代表用以代表这个颜色范围
         this.__pool = new Map();
         if (child) {
+            this.concat(child)
             // 代理一些Color类上的方法和属性，注意方法是否需要bind
             let proxyList = new Set(['value', 'data', 'isDeep', 'toColorString'])
             return new Proxy(this, {
@@ -28,7 +29,6 @@ class ColorGroup extends Count {
                     }
                 }
             })
-            this.concat(child)
         }
     }
     /**
@@ -40,11 +40,10 @@ class ColorGroup extends Count {
             this.__pool.set(color.value, color);
             this.append(color)
         } else if (color.isGroup){
-            this.__pool.set(color.proxy.value, color);
+            this.__pool.set(color.__proxy.value, color);
             this.append(color)
         } else {
-            this.__groupPool.set(color.proxy.value, color);
-            this.append(color)
+            throw('数据错误', color)
         }
     }
     /**
@@ -57,17 +56,14 @@ class ColorGroup extends Count {
         if (color instanceof Color) {
             let data = pool.get(color.value)
             if (data) {
-                // 如果传入的是color 则 option可传入的为count
-                data.plus(color.count)
-                this.plus(color.count)
+                data.plus(color.__count)
             } else {
                 this.pushPool(color)
             }
         } else if (color.isGroup) {
-            let data = pool.get(color.proxy.value)
+            let data = pool.get(color.__proxy.value)
             if (data) {
-                data.plus(color.count)
-                this.plus(color.count)
+                data.plus(color.__count)
             } else {
                 this.pushPool(color)
             }
@@ -76,7 +72,6 @@ class ColorGroup extends Count {
             let data = pool.get(colorString)
             if (data) {
                 data.plus()
-                this.plus()
             } else {
                 color = new Color(color, utils.paramsFilter.color({
                     ...option,
@@ -113,17 +108,20 @@ class ColorGroup extends Count {
     createColorProxy(option = {}, colorGroup) {
         colorGroup = colorGroup || this;
         if (colorGroup.__proxy) return this;
-        let {colorStep} = option;
+        let {colorStep = 100} = option;
         let map = new Set();
         colorGroup.sortList.forEach(item => {
             let hasNoColor = true;
             map.forEach((value, key) => {
-                if (utils.isSimilarColor(value.proxy, item, colorStep || 100)) {
+                if (utils.isSimilarColor(value.__proxy, item, colorStep) && hasNoColor) {
+                    // 需要注意避免被重复判定为相似颜色
                     hasNoColor = false
-                    value.concat(item)
+                    item = Color.clone(item)
+                    value.concat(item, value);
                 }
             })
             if (hasNoColor) {
+                item = Color.clone(item)
                 map.add(new ColorGroup(item))
             }
         })
@@ -137,25 +135,13 @@ class ColorGroup extends Count {
     get proxy() {
         return this.__proxy
     }
-    /**
-     * 不允许设置
-     */
-    // set proxy(color) {
-    //     if (!this.__proxy) {
-    //         this.__proxy = color
-    //     }
-    // }
-
-    get value() {
-        return  this.proxy && this.proxy.value
-    }
 
     get list() {
         return Array.from(this.__pool.values())
     }
 
     get sortList() {
-        return this.list.sort((item1, item2) => item2.count - item1.count)
+        return this.list.sort((item1, item2) => item2.__count - item1.__count)
     }
 }
 
